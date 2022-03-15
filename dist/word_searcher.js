79f5f0e54ac1bed6dce4289d2f080b4d4fd18c6e
mitty1293 <k.fmichi@gmail.com>
2022-03-15 23:38:42 +0900

    動作確認まで完了

--- dist/word_searcher.js
+++ dist/word_searcher.js
@@ -1,5 +1,18 @@
 export const word_searcher = (pattern) => {
-    const test_data = "121-4440";
-    const result = test_data.match(pattern);
-    return result;
+    const test_data = [
+        "121-4440",
+        "527-0231",
+        "5555-3333",
+        "111-1111",
+        "2222-222"
+    ];
+    const match_array = [];
+    for (const data of test_data) {
+        const result = data.match(pattern);
+        if (result) {
+            match_array.push(result[0]);
+        }
+        console.log(typeof (result));
+    }
+    return match_array;
 };
--- ts/word_searcher.ts
+++ ts/word_searcher.ts
@@ -1,5 +1,19 @@
-export const word_searcher = (pattern: string): string => {
-    const test_data: string = "121-4440";
-    const result = test_data.match(pattern);
-    return result;
+export const word_searcher = (pattern: RegExp): RegExpMatchArray | null => {
+    const test_data: string[] = [
+        "121-4440",
+        "527-0231",
+        "5555-3333",
+        "111-1111",
+        "2222-222"
+    ];
+    const match_array: string[] = [];
+
+    for (const data of test_data) {
+        const result: RegExpMatchArray | null = data.match(pattern);
+        if (result) {
+            match_array.push(result[0]);
+        }
+    }
+
+    return match_array;
 };
